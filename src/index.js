13e793c1145a34c75acb9675fc22e6f1aa73230e
Ivan Zeleneckiy <izeleneckiy@gmail.com>
2020-09-30 11:19:07 +0400

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if ((array == undefined) || (array.length == 0)) return 0;
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min) min = array[i];
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if ((array == undefined) || (array.length == 0)) return 0;
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) max = array[i];
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if ((array == undefined) || (array.length == 0)) return 0;
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += array[i];
+    }
+    return result / array.length;
 }
